a3db014e862ecdfb45eb545f02e8b24b1bd958bf
Sara Amiry <saraamiry24@gmail.com>
2020-04-23 23:22:23 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  name_hash.each do |key, value|
+    puts key + " => " + value
+  end
 end
+
+key_for_min_value({:blake => 500, :ashley => 2, :adam => 1})
+
 user = {
   :name => "steve",
   :location => "idaho",
